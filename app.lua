e12305a14725dd9900a9679d8bf02122dd749a35
EletrixTime <71174682+eletrixtime@users.noreply.github.com>
2024-08-30 21:53:29 +0200

    V1
    v1

---
+++ app.lua
@@ -0,0 +1,56 @@
+
+ROOT_FOLDER = "/" -- debug only
+ATMP = "/"
+function ShowFiles(path)
+    local list = gui:vlist(win, 35, 90, 250, 280)
+    local files = storage:listDir(path)
+    for i, file in ipairs(files) do
+        print(file)
+        local case = gui:box(list, 0, (i-1)*36, 250, 36)
+        local nameLabel = gui:label(case, 21, 0, 230, 18)
+        nameLabel:setText(file)
+        nameLabel:setFontSize(16)
+        case:onClick(function()
+            print("[EXPLORER]: " .. file)
+            -- Check if a directory (if end with /)
+            
+            ShowFiles(path .. "/" .. file)
+            ATMP = path
+          
+        end)
+    end
+end
+function back()
+    ShowFiles(ATMP)
+end
+function run(arg)
+    print("[EXPLORER] : Loaded !")
+
+
+    win=gui:window()
+    local title=gui:label(win, 0, 0, 144, 28)
+
+    local back_button = gui:box(win, 250, 410, 40, 40)
+    back_button:setMainColor(COLOR_DARK)
+    back_button:setRadius(20)
+    
+    local back_icon = gui:image(back_button, "assets/app_assets/back.png", 14, 14, 12, 12)
+    back_button:onClick(back)
+    
+
+
+    title:setFontSize(10)
+    title:setText("[File Explorer] (by eletrixtime)")
+    ShowFiles("assets")
+--    local info_icon = gui:box(win, 250, 410, 40, 40)
+--    info_icon:setMainColor(COLOR_DARK)
+--    info_icon:setRadius(20)
+--    local info_icon_ic = gui:image(info_icon, "assets/info.png", 14, 14, 12, 12, COLOR_DARK)
+--    info_icon:onClick(function ()
+--        print("Infooo")
+--    end)
+    
+    
+
+    gui:setWindow(win)
+end
